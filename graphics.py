bd8179861caf711fd94fa33e83f4473bbce68d0b
FlxdeCat <felix.ironcat59@gmail.com>
2024-02-13 16:43:02 +0700

    feat snap tiles

--- graphics.py
+++ graphics.py
@@ -2,78 +2,8 @@ from tkinter import *
 from tkinter import ttk
 from constants import *
 
-class DragTileManager():
-    def __init__(self, frame):
-        self.tile = None
-        self.start_x = 0
-        self.start_y = 0
-        self.frame = frame
-
-    def add_tile(self, tile):
-        tile.bind("<ButtonPress-1>", self.on_drag_start)
-        tile.bind("<B1-Motion>", self.on_drag)
-        tile.bind("<ButtonRelease-1>", self.on_drag_end)
-        tile.configure(cursor="hand1")
-
-    def on_drag_start(self, event):
-        self.tile = event.widget
-        self.start_x = event.x
-        self.start_y = event.y
-
-    def on_drag(self, event):
-        if self.tile:
-            x = event.widget.winfo_x() - self.start_x + event.x
-            y = event.widget.winfo_y() - self.start_y + event.y
-
-            board_width = self.frame.winfo_width()
-            board_height = self.frame.winfo_height()
-            tile_width = self.tile.winfo_width()
-            tile_height = self.tile.winfo_height()
-
-            x = min(max(x, 0) - 9, board_width - tile_width - 12)
-            y = min(max(y, 0) - 9, board_height - tile_height - 12)
-
-            event.widget.place_configure(x=x, y=y)
-
-    def on_drag_end(self, event):
-        self.tile = None
-
-window = Tk()
-style = ttk.Style()
-window.resizable(0, 0)
-window.title("Scrabble")
-window.geometry("%dx%d%+d%+d" % (700, 750, 0, 0))
-
-board_frame = ttk.Frame(window)
-board_frame['padding'] = 5
-board_frame['borderwidth'] = 5
-board_frame['relief'] = 'groove'
-bar_frame = ttk.Frame(window)
-btn_frame = ttk.Frame(window)
-
-drag_tile_manager = DragTileManager(board_frame)
-
-board_frame.place(relx=0.5, rely=0.45, anchor = CENTER)
-bar_frame.place(x = 0, rely = 0.85, relheight = 0.05, relwidth = 1)
-btn_frame.place(x = 0, rely = 0.9, relheight = 0.05, relwidth = 1)
-
-btn_frame.columnconfigure((0, 1, 2, 3), weight = 1, uniform = "a")
-btn_frame.rowconfigure((0), weight = 1, uniform = "a")
-
-style.configure("Bar.TButton", font=("Ubuntu", 10))
-shuffle_btn = ttk.Button(btn_frame, text = "Shuffle", style="Bar.TButton")
-exchange_btn = ttk.Button(btn_frame, text = "Exchange", style="Bar.TButton")
-submit_btn = ttk.Button(btn_frame, text = "Submit", style="Bar.TButton")
-quit_btn = ttk.Button(btn_frame, text = "Quit", style="Bar.TButton")
-
-shuffle_btn.grid(row = 0, column = 0)
-exchange_btn.grid(row = 0, column = 1)
-submit_btn.grid(row = 0, column = 2)
-quit_btn.grid(row = 0, column = 3)
-
-style.configure("Turn.TLabel", font = ("Helvetica", 15, "bold"))
-turn_lbl = ttk.Label(bar_frame, text = "Your Turn!", style = "Turn.TLabel", anchor = "center")
-turn_lbl.pack()
+tile_size = 30
+square_size = 35
 
 board = [
         ["TW", "RT", "RT", "DL", "RT", "RT", "RT", "TW", "RT", "RT", "RT", "DL", "RT", "RT", "TW"],
@@ -119,8 +49,137 @@ board_colors = {
     "TW": "orange"
 }
 
-tile_size = 30
-square_size = 35
+hand = ["Z", "Q", "J", "Y", "Q", "K", "Z"]
+
+class DragTileManager():
+    def __init__(self, frame):
+        self.tile = None
+        self.start_x = 0
+        self.start_y = 0
+        self.frame = frame
+
+    def add_tile(self, tile):
+        tile.bind("<ButtonPress-1>", self.on_drag_start)
+        tile.bind("<B1-Motion>", self.on_drag)
+        tile.bind("<ButtonRelease-1>", self.on_drag_end)
+        tile.configure(cursor="hand1")
+
+    def on_drag_start(self, event):
+        self.tile = event.widget
+        self.ori_x = event.widget.winfo_x()
+        self.ori_y = event.widget.winfo_y()
+        self.start_x = event.x
+        self.start_y = event.y
+
+    def on_drag(self, event):
+        if self.tile:
+            x = event.widget.winfo_x() - self.start_x + event.x
+            y = event.widget.winfo_y() - self.start_y + event.y
+
+            board_width = self.frame.winfo_width()
+            board_height = self.frame.winfo_height()
+
+            x = min(max(x, 0) - 9, board_width - 18)
+            y = min(max(y, 0) - 9, board_height - 18)
+
+            event.widget.place_configure(x=x, y=y)
+
+    def on_drag_end(self, event):
+        if self.tile:
+            x = event.widget.winfo_x()
+            y = event.widget.winfo_y()
+
+            board_width = self.frame.winfo_width()
+            board_height = self.frame.winfo_height()
+            grid_col = min(x // square_size, 14)
+            grid_row = min(y // square_size, 14)
+
+            if(y // square_size == 15 and x // square_size >= 4 and x // square_size <= 10):
+                grid_col = x // square_size
+                grid_row = 15
+
+                print("Tile placed at position:", grid_col, grid_row)
+
+                snap_x = grid_col * square_size
+                snap_y = grid_row * square_size
+
+                snap_x = min(max(snap_x, 0), board_width - tile_size)
+                snap_y = min(max(snap_y, 0), board_height - tile_size)
+
+                snap_x += 2.5
+                snap_y += 12.5
+
+                event.widget.place_configure(x=snap_x, y=snap_y)
+            else:
+                print("Tile placed at position:", grid_col, grid_row)
+                
+                if(tile[grid_row][grid_col] != "-"):
+                    grid_col = self.ori_x // square_size
+                    grid_row = self.ori_y // square_size
+
+                    snap_x = grid_col * square_size
+                    snap_y = grid_row * square_size
+
+                    snap_x = min(max(snap_x, 0), board_width - tile_size)
+                    snap_y = min(max(snap_y, 0), board_height - tile_size)
+
+                    snap_x += 2.5
+                    if(self.ori_y // square_size == 15):
+                        snap_y += 12.5
+                    else:
+                        snap_y += 2.5
+
+                    event.widget.place_configure(x=snap_x, y=snap_y)
+                else:
+                    snap_x = grid_col * square_size
+                    snap_y = grid_row * square_size
+
+                    snap_x = min(max(snap_x, 0), board_width - tile_size)
+                    snap_y = min(max(snap_y, 0), board_height - tile_size)
+
+                    snap_x += 2.5
+                    snap_y += 2.5
+
+                    event.widget.place_configure(x=snap_x, y=snap_y)
+        self.tile = None
+
+window = Tk()
+style = ttk.Style()
+window.resizable(0, 0)
+window.title("Scrabble")
+window.geometry("%dx%d%+d%+d" % (700, 750, 0, 0))
+
+board_frame = ttk.Frame(window)
+board_frame['padding'] = 5
+board_frame['borderwidth'] = 5
+board_frame['relief'] = 'groove'
+bar_frame = ttk.Frame(window)
+btn_frame = ttk.Frame(window)
+
+drag_tile_manager = DragTileManager(board_frame)
+
+board_frame.place(relx=0.5, rely=0.45, anchor = CENTER)
+bar_frame.place(x = 0, rely = 0.85, relheight = 0.05, relwidth = 1)
+btn_frame.place(x = 0, rely = 0.9, relheight = 0.05, relwidth = 1)
+
+btn_frame.columnconfigure((0, 1, 2, 3), weight = 1, uniform = "a")
+btn_frame.rowconfigure((0), weight = 1, uniform = "a")
+
+style.configure("Bar.TButton", font=("Ubuntu", 10))
+shuffle_btn = ttk.Button(btn_frame, text = "Shuffle", style="Bar.TButton")
+exchange_btn = ttk.Button(btn_frame, text = "Exchange", style="Bar.TButton")
+submit_btn = ttk.Button(btn_frame, text = "Submit", style="Bar.TButton")
+quit_btn = ttk.Button(btn_frame, text = "Quit", style="Bar.TButton")
+
+shuffle_btn.grid(row = 0, column = 0)
+exchange_btn.grid(row = 0, column = 1)
+submit_btn.grid(row = 0, column = 2)
+quit_btn.grid(row = 0, column = 3)
+
+style.configure("Turn.TLabel", font = ("Helvetica", 15, "bold"))
+turn_lbl = ttk.Label(bar_frame, text = "Your Turn!", style = "Turn.TLabel", anchor = "center")
+turn_lbl.pack()
+
 for row in range(len(board)):
     for col in range(len(board[row])):
         tile_type = board[row][col]
@@ -142,8 +201,6 @@ for row in range(len(tile)):
             square.create_text(tile_size // 2, tile_size // 2, text=tile[row][col], font=("Helvetica", 10))
             square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
             square.grid(row=row, column=col)
-
-hand = ["Z", "Q", "J", "Y", "Q", "K", "Z"]
 
 board_frame.grid_rowconfigure(16, minsize=10)
 
