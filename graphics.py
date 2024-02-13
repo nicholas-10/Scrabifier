f2797d179b1fb81bd7ab00da691d3bdfdf56c31d
FlxdeCat <felix.ironcat59@gmail.com>
2024-02-13 15:37:27 +0700

    moving tiles fix

--- graphics.py
+++ graphics.py
@@ -2,15 +2,12 @@ from tkinter import *
 from tkinter import ttk
 from constants import *
 
-from tkinter import Canvas
-Canvas.tag_raise = Canvas.tkraise
-del Canvas.tkraise, Canvas.lift
-
 class DragTileManager():
-    def __init__(self):
+    def __init__(self, frame):
         self.tile = None
         self.start_x = 0
         self.start_y = 0
+        self.frame = frame
 
     def add_tile(self, tile):
         tile.bind("<ButtonPress-1>", self.on_drag_start)
@@ -22,35 +19,43 @@ class DragTileManager():
         self.tile = event.widget
         self.start_x = event.x
         self.start_y = event.y
-        self.tile.lift()
-        self.tile.tk.call("raise", self.tile)
 
     def on_drag(self, event):
         if self.tile:
             x = event.widget.winfo_x() - self.start_x + event.x
             y = event.widget.winfo_y() - self.start_y + event.y
+
+            board_width = self.frame.winfo_width()
+            board_height = self.frame.winfo_height()
+            tile_width = self.tile.winfo_width()
+            tile_height = self.tile.winfo_height()
+
+            x = min(max(x, 0) - 9, board_width - tile_width - 12)
+            y = min(max(y, 0) - 9, board_height - tile_height - 12)
+
             event.widget.place_configure(x=x, y=y)
 
     def on_drag_end(self, event):
         self.tile = None
 
-drag_tile_manager = DragTileManager()
-
 window = Tk()
 style = ttk.Style()
 window.resizable(0, 0)
 window.title("Scrabble")
-window.geometry("%dx%d%+d%+d" % (700, 700, 0, 0))
+window.geometry("%dx%d%+d%+d" % (700, 750, 0, 0))
 
 board_frame = ttk.Frame(window)
+board_frame['padding'] = 5
+board_frame['borderwidth'] = 5
+board_frame['relief'] = 'groove'
 bar_frame = ttk.Frame(window)
-tile_frame = ttk.Frame(window)
 btn_frame = ttk.Frame(window)
 
-board_frame.place(relx=0.5, rely=0.4, anchor = CENTER)
-bar_frame.place(x = 0, rely = 0.8, relheight = 0.05, relwidth = 1)
-tile_frame.place(x = 0, rely = 0.85, relheight = 0.05, relwidth = 1)
-btn_frame.place(x = 0, rely = 0.9, relheight = 0.1, relwidth = 1)
+drag_tile_manager = DragTileManager(board_frame)
+
+board_frame.place(relx=0.5, rely=0.45, anchor = CENTER)
+bar_frame.place(x = 0, rely = 0.85, relheight = 0.05, relwidth = 1)
+btn_frame.place(x = 0, rely = 0.9, relheight = 0.05, relwidth = 1)
 
 btn_frame.columnconfigure((0, 1, 2, 3), weight = 1, uniform = "a")
 btn_frame.rowconfigure((0), weight = 1, uniform = "a")
@@ -140,18 +145,17 @@ for row in range(len(tile)):
 
 hand = ["Z", "Q", "J", "Y", "Q", "K", "Z"]
 
-tile_frame.grid_columnconfigure(0, weight = 1)
-tile_frame.grid_columnconfigure(9, weight = 1)
+board_frame.grid_rowconfigure(16, minsize=10)
 
 for col in range(7):
-    square = Canvas(tile_frame, width=square_size, height=square_size, bg='beige', highlightthickness=0)
+    square = Canvas(board_frame, width=square_size, height=square_size, bg='beige', highlightthickness=0)
     square.create_rectangle(0, 0, square_size, square_size, outline="black")
-    square.grid(row=0, column=col+1)
+    square.grid(row=17, column=col+4)
 
 for col, letter in enumerate(hand):
-    square = Canvas(tile_frame, width=tile_size, height=tile_size, bg='burlywood', highlightthickness=0)
+    square = Canvas(board_frame, width=tile_size, height=tile_size, bg='burlywood', highlightthickness=0)
     square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
-    square.grid(row=0, column=col+1)
+    square.grid(row=17, column=col+4)
     score = SCRABBLE_LETTER_POINTS.get(letter.upper(), 0)
     square.create_text(tile_size - 6, tile_size - 6, text=f"{score}", font=("Helvetica", 6))
     square.create_text(tile_size // 2, tile_size // 2, text=f"{letter}", font=("Helvetica", 10))
