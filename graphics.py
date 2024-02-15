fea372d0fab61cc4dfb8c740a20a89a55d2afd12
FlxdeCat <felix.ironcat59@gmail.com>
2024-02-15 14:30:33 +0700

    feat: shuffle

--- graphics.py
+++ graphics.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from constants import *
+import random
+import copy
 
 tile_size = 30
 square_size = 35
@@ -23,7 +25,7 @@ board = [
         ["TW", "RT", "RT", "DL", "RT", "RT", "RT", "TW", "RT", "RT", "RT", "DL", "RT", "RT", "TW"]
         ]
 
-tile = [
+ori_tile = [
         ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
         ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "?", "R", "E", "S", "S"],
         ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O", "-", "-", "-", "-"],
@@ -40,6 +42,7 @@ tile = [
         ["?", "R", "A", "G", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
         ["S", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
         ]
+tile = copy.deepcopy(ori_tile)
 
 board_colors = {
     "CT": "deeppink1",
@@ -49,7 +52,8 @@ board_colors = {
     "TW": "orange"
 }
 
-hand = ["Z", "Q", "J", "Y", "Q", "K", "Z"]
+ori_hand = ["Z", "Q", "J", "Y", "Q", "K", "Z"]
+hand = ori_hand.copy()
 
 class DragTileManager():
     def __init__(self, frame):
@@ -125,6 +129,20 @@ class DragTileManager():
             event.widget.place_configure(x=snap_x, y=snap_y)
             self.tile = None
 
+def reset():
+    for widget in board_frame.winfo_children():
+        widget.destroy()
+    for widget in bar_frame.winfo_children():
+        widget.destroy()
+
+def shuffle_hand():
+    global hand, ori_hand, tile, ori_tile, board_frame, bar_frame
+    random.shuffle(ori_hand)
+    hand = ori_hand.copy()
+    tile = copy.deepcopy(ori_tile)
+    reset()
+    open_window()
+
 window = Tk()
 style = ttk.Style()
 window.resizable(0, 0)
@@ -148,7 +166,7 @@ btn_frame.columnconfigure((0, 1, 2, 3), weight = 1, uniform = "a")
 btn_frame.rowconfigure((0), weight = 1, uniform = "a")
 
 style.configure("Bar.TButton", font=("Ubuntu", 10))
-shuffle_btn = ttk.Button(btn_frame, text = "Shuffle", style="Bar.TButton")
+shuffle_btn = ttk.Button(btn_frame, text = "Shuffle", style="Bar.TButton", command=shuffle_hand)
 exchange_btn = ttk.Button(btn_frame, text = "Exchange", style="Bar.TButton")
 submit_btn = ttk.Button(btn_frame, text = "Submit", style="Bar.TButton")
 quit_btn = ttk.Button(btn_frame, text = "Quit", style="Bar.TButton")
@@ -162,43 +180,47 @@ style.configure("Turn.TLabel", font = ("Helvetica", 15, "bold"))
 turn_lbl = ttk.Label(bar_frame, text = "Your Turn!", style = "Turn.TLabel", anchor = "center")
 turn_lbl.pack()
 
-for row in range(len(board)):
-    for col in range(len(board[row])):
-        tile_type = board[row][col]
-        color = board_colors.get(tile_type, "beige") 
-        square = Canvas(board_frame, width=square_size, height=square_size, bg=color, highlightthickness=0)
-        if(board[row][col] != "RT" and board[row][col] != "CT"):
-            square.create_text(square_size // 2, square_size // 2, text=board[row][col], font=("Helvetica", 10))
-        square.create_rectangle(0, 0, square_size, square_size, outline="black")
-        square.grid(row=row, column=col)
-
-for row in range(len(tile)):
-    for col in range(len(tile[row])):
-        if(tile[row][col] != "-"):
+def open_window():
+    global hand, ori_hand, tile, ori_tile, window, board_frame, bar_frame, btn_frame, drag_tile_manager
+    for row in range(len(board)):
+        for col in range(len(board[row])):
             tile_type = board[row][col]
             color = board_colors.get(tile_type, "beige") 
-            square = Canvas(board_frame, width=tile_size, height=tile_size, bg="burlywood1", highlightthickness=0)
-            score = SCRABBLE_LETTER_POINTS.get(tile[row][col], 0)
-            square.create_text(tile_size - 6, tile_size - 6, text=f"{score}", font=("Helvetica", 6))
-            square.create_text(tile_size // 2, tile_size // 2, text=tile[row][col], font=("Helvetica", 10))
-            square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
+            square = Canvas(board_frame, width=square_size, height=square_size, bg=color, highlightthickness=0)
+            if(board[row][col] != "RT" and board[row][col] != "CT"):
+                square.create_text(square_size // 2, square_size // 2, text=board[row][col], font=("Helvetica", 10))
+            square.create_rectangle(0, 0, square_size, square_size, outline="black")
             square.grid(row=row, column=col)
 
-board_frame.grid_rowconfigure(16, minsize=10)
+    for row in range(len(tile)):
+        for col in range(len(tile[row])):
+            if(tile[row][col] != "-"):
+                tile_type = board[row][col]
+                color = board_colors.get(tile_type, "beige") 
+                square = Canvas(board_frame, width=tile_size, height=tile_size, bg="burlywood1", highlightthickness=0)
+                score = SCRABBLE_LETTER_POINTS.get(tile[row][col], 0)
+                square.create_text(tile_size - 6, tile_size - 6, text=f"{score}", font=("Helvetica", 6))
+                square.create_text(tile_size // 2, tile_size // 2, text=tile[row][col], font=("Helvetica", 10))
+                square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
+                square.grid(row=row, column=col)
 
-for col in range(7):
-    square = Canvas(board_frame, width=square_size, height=square_size, bg='beige', highlightthickness=0)
-    square.create_rectangle(0, 0, square_size, square_size, outline="black")
-    square.grid(row=17, column=col+4)
+    board_frame.grid_rowconfigure(16, minsize=10)
 
-for col, letter in enumerate(hand):
-    square = Canvas(board_frame, width=tile_size, height=tile_size, bg='burlywood', highlightthickness=0)
-    square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
-    square.grid(row=17, column=col+4)
-    score = SCRABBLE_LETTER_POINTS.get(letter.upper(), 0)
-    square.create_text(tile_size - 6, tile_size - 6, text=f"{score}", font=("Helvetica", 6))
-    square.create_text(tile_size // 2, tile_size // 2, text=f"{letter}", font=("Helvetica", 10))
-    square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
-    drag_tile_manager.add_tile(square, letter)
+    for col in range(7):
+        square = Canvas(board_frame, width=square_size, height=square_size, bg='beige', highlightthickness=0)
+        square.create_rectangle(0, 0, square_size, square_size, outline="black")
+        square.grid(row=17, column=col+4)
 
-window.mainloop()
+    for col, letter in enumerate(hand):
+        square = Canvas(board_frame, width=tile_size, height=tile_size, bg='burlywood', highlightthickness=0)
+        square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
+        square.grid(row=17, column=col+4)
+        score = SCRABBLE_LETTER_POINTS.get(letter.upper(), 0)
+        square.create_text(tile_size - 6, tile_size - 6, text=f"{score}", font=("Helvetica", 6))
+        square.create_text(tile_size // 2, tile_size // 2, text=f"{letter}", font=("Helvetica", 10))
+        square.create_rectangle(0, 0, tile_size, tile_size, outline="black")
+        drag_tile_manager.add_tile(square, letter)
+
+    window.mainloop()
+
+open_window()
